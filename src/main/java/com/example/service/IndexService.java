f7f6894661cd931c19999e2c17045435f6a9f6a9
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-07-14 16:06:46 +0900

    IndexServiceの作成（findAllメソッド）

--- src/main/java/com/example/service/IndexService.java
+++ src/main/java/com/example/service/IndexService.java
@@ -1,5 +1,27 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Item;
+import com.example.repository.ItemsRepository;
+
+@Service
+@Transactional
 public class IndexService {
 
+	@Autowired
+	private ItemsRepository itemRepository;
+	
+	/**
+	 * 全件検索を行う。
+	 * @return 全アイテム一覧
+	 */
+	public List<Item> findAll(){
+		List<Item> itemList = itemRepository.findAll();
+		return itemList;
+	}
 }
