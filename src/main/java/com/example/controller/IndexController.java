b0e70b38503fc56696acc389462e0afe82646aee
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-07-14 16:08:10 +0900

    IndexControllerの作成（indexメソッド)

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,5 +1,26 @@
 package com.example.controller;
 
-public class IndexController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Item;
+import com.example.service.IndexService;
+
+@Controller
+@RequestMapping("/item-list")
+public class IndexController {
+	
+	@Autowired
+	private IndexService indexService;
+
+	@RequestMapping("")
+	public String index(Model model) {
+		List<Item> itemList = indexService.findAll();
+		model.addAttribute("itemList", itemList);
+		return "item/item_list";
+	}
 }
