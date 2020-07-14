05ad6139bf9e74cfffba8e026d8bca53914dfa2d
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2020-07-14 18:06:06 +0900

    findById()itemIdで商品を見つけるメソッドを作成

--- src/main/java/com/example/repository/ItemsRepository.java
+++ src/main/java/com/example/repository/ItemsRepository.java
@@ -1,13 +1,16 @@
 package com.example.repository;
 
 import java.util.List;
+import java.util.Objects;
+
+import com.example.domain.Item;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
-
-import com.example.domain.Item;
 
 @Repository
 public class ItemsRepository {
@@ -41,5 +44,15 @@ public class ItemsRepository {
 		List<Item> itemList = template.query(sql, ITEM_ROW_MAPPER);
 
 		return itemList;
+	}
+
+	public  Item findById(String id) {
+		String sql="SELECT * FROM items WHERE id=:id";
+		SqlParameterSource param=new MapSqlParameterSource().addValue("id", id);
+		Item item=template.queryForObject(sql, param,ITEM_ROW_MAPPER);
+		if(Objects.isNull(item)){
+			return null;
+		}
+		return item;
 	}
 }
