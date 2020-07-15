ea0fb61218150a3e1d4a7d62eea368e6ab2f6d7d
sawaguchiarisa <arisa.sawaguchi@rakus-partners.co.jp>
2020-07-15 12:34:14 +0900

    InsertController追加

--- src/main/java/com/example/controller/InsertUserController.java
+++ src/main/java/com/example/controller/InsertUserController.java
@@ -1,5 +1,77 @@
 package com.example.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.User;
+import com.example.form.InsertUserForm;
+import com.example.service.InsertUserService;
+
+@Controller
+@RequestMapping("/")
 public class InsertUserController {
 
+	@Autowired
+	private InsertUserService insertUserService;
+	
+	@Autowired
+	private HttpSession session;
+	
+	@ModelAttribute
+	public InsertUserForm setUpInsertUserForm() {
+		return new InsertUserForm();
+	}
+	 /////////////////////////////////////////////////////
+	  // ユースケース：管理者を登録する
+	  /////////////////////////////////////////////////////
+	  /**
+	  * 管理者登録画面を出力します.
+	  * 
+	  * @return 管理者登録画面
+	  */
+	  @RequestMapping("/toInsert")
+	  public String toInsert() {
+	  return "user/register_user";
+	  }
+	  /**
+	   * 
+	   * @param insertUserForm
+	   * @return item/item_listにフォワード
+	   */
+	  @RequestMapping("/insert")
+	  public String insert(@Validated InsertUserForm insertUserForm, BindingResult result, Model model) {
+		  if (result.hasErrors()) {
+			return toInsert();
+		}
+		  User user = new User();
+		  user.setName(insertUserForm.getName());
+		  user.setEmail(insertUserForm.getEmail());
+		  user.setPassword(insertUserForm.getPassword());
+		  StringBuilder br = new StringBuilder(insertUserForm.getZipcode());
+			br.deleteCharAt(3);
+		  user.setZipcode(br.toString());
+		  user.setAddress(insertUserForm.getAddress());
+		  user.setTelephone(insertUserForm.getTelephone());
+			
+		  //メールアドレスの重複確認
+		  if (insertUserService.findByMailAddress(insertUserForm.getEmail())!= null) {
+			model.addAttribute("emailError", "そのメールアドレスはすでに使われています");
+			return toInsert();
+		}
+		  //確認用パスワード
+		  if (insertUserForm.getPassword().equals(insertUserForm.getCheckpassword())) {
+			  insertUserService.insert(user);
+		}else {
+			model.addAttribute("passwordError","パスワードと確認用パスワードが不一致です");
+			return toInsert();
+		}
+		  return "item/item_list";
+	  }
 }
