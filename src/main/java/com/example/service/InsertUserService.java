b29d9893cc8cc8f8b470d4d7ae54aaaf3ac7636c
sawaguchiarisa <arisa.sawaguchi@rakus-partners.co.jp>
2020-07-15 12:36:33 +0900

    InsertUserservice追加

--- src/main/java/com/example/service/InsertUserService.java
+++ src/main/java/com/example/service/InsertUserService.java
@@ -1,5 +1,27 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.User;
+import com.example.repository.UsersRepository;
+
+@Service
+@Transactional
 public class InsertUserService {
 
+	@Autowired
+	private UsersRepository usersRepository;
+	
+	public void insert(User user) {
+		StringBuilder br = new StringBuilder(user.getZipcode());
+		br.deleteCharAt(3);
+
+		usersRepository.insert(user);
+	}
+	
+	public User findByMailAddress(String email) {
+		return usersRepository.findByMailAddress(email);
+	}
 }
