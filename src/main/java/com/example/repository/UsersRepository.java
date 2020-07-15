1ae40da16a1d6cbf2faf3e04145450d8d50a0ae7
sawaguchiarisa <arisa.sawaguchi@rakus-partners.co.jp>
2020-07-15 13:54:13 +0900

    JavaDockコメント追加

--- src/main/java/com/example/repository/UsersRepository.java
+++ src/main/java/com/example/repository/UsersRepository.java
@@ -27,35 +27,47 @@ public class UsersRepository {
 		user.setTelephone(rs.getString("telephone"));
 		return user;
 	};
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate template;
-	
+
 	/**
 	 * 
 	 * @param email
 	 * @param password
-	 * @return userList
-	 * メールアドレスとパスワードからログイン
+	 * @return userList メールアドレスとパスワードからログイン
 	 */
 	public User findByMailAddressAndPassward(String email, String password) {
 		String sql = "select * from users where email=:email and password=:password";
-		SqlParameterSource param = new MapSqlParameterSource().addValue("email", email).addValue("password",password);
+		SqlParameterSource param = new MapSqlParameterSource().addValue("email", email).addValue("password", password);
 		List<User> userList = template.query(sql, param, USER_ROW_MAPPER);
 		if (userList.size() == 0) {
 			return null;
 		}
 		return userList.get(0);
 	}
+
+	/**
+	 * 登録画面に名前、メールアドレス、パスワード、郵便番号、住所、電話番号をインサート
+	 * 
+	 * @param user
+	 */
 	public void insert(User user) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(user);
 		String insertSql = "INSERT INTO users(name,email,password,zipcode,address,telephone) VALUES(:name,:email,:password,:zipcode,:address,:telephone)";
 		template.update(insertSql, param);
 	}
+
+	/**
+	 * メールアドレスの重複確認
+	 * 
+	 * @param email
+	 * @return
+	 */
 	public User findByMailAddress(String email) {
 		String sql = "select * from users where email = :email";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("email", email);
-		List<User> userList = template.query(sql, param,USER_ROW_MAPPER);
+		List<User> userList = template.query(sql, param, USER_ROW_MAPPER);
 		if (userList.size() == 0) {
 			return null;
 		}
