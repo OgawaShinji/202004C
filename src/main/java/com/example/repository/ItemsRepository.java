85d2675f2471aa660585c4023ffdac5a94c3d5cc
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-07-15 12:09:03 +0900

    曖昧検索メソッド

--- src/main/java/com/example/repository/ItemsRepository.java
+++ src/main/java/com/example/repository/ItemsRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Item;
@@ -29,20 +31,37 @@ public class ItemsRepository {
 	};
 
 	/**
-	 * 全件検索を行う。
+	 * 全件検索を行う.
 	 * 
 	 * @return 全アイテム一覧
 	 */
 	public List<Item> findAll() {
 		// 表示確認を優先する為、toppingsのJOINはまだしていません。
 		// Mサイズの価格が安い順で表示されるようにしています。
-		String sql = "SELECT id,name,description,price_m,price_l,image_path,deleted FROM items ORDER BY price_m";
+		String sql = "SELECT id,name,description,price_m,price_l,image_path,deleted FROM items WHERE deleted != true ORDER BY price_m";
 
 		List<Item> itemList = template.query(sql, ITEM_ROW_MAPPER);
-		
+
 		if (itemList.size() == 0) {
 			return null;
 		}
+
+		return itemList;
+	}
+
+	/**
+	 * 名前からアイテムを曖昧検索する.
+	 * 
+	 * @param name 名前
+	 * @return 検索されたアイテム一覧
+	 */
+	public List<Item> findByLikeName(String name) {
+		String sql = "SELECT id,name,description,price_m,price_l,image_path,deleted FROM items"
+				+ " WHERE name LIKE :name AND deleted != true ORDER BY price_m";
+
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%" + name + "%");
+
+		List<Item> itemList = template.query(sql, param, ITEM_ROW_MAPPER);
 
 		return itemList;
 	}
