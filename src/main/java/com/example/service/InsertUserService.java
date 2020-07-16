06f52a2cadfd094c0d60ac30d6a0505991335448
sawaguchiarisa <arisa.sawaguchi@rakus-partners.co.jp>
2020-07-16 17:11:06 +0900

    微々修正

--- src/main/java/com/example/service/InsertUserService.java
+++ src/main/java/com/example/service/InsertUserService.java
@@ -13,14 +13,12 @@ public class InsertUserService {
 
 	@Autowired
 	private UsersRepository usersRepository;
-	
+
 	public void insert(User user) {
-		StringBuilder br = new StringBuilder(user.getZipcode());
-		br.deleteCharAt(3);
 
 		usersRepository.insert(user);
 	}
-	
+
 	public User findByMailAddress(String email) {
 		return usersRepository.findByMailAddress(email);
 	}
