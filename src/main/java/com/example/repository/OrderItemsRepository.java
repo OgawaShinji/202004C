4ce9260d5fd9843b02a1046e2a7d6d154475ffaa
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-07-16 10:03:52 +0900

    add method of insert for add shoppingCart

--- src/main/java/com/example/repository/OrderItemsRepository.java
+++ src/main/java/com/example/repository/OrderItemsRepository.java
@@ -1,5 +1,26 @@
 package com.example.repository;
 
+import com.example.domain.OrderItem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class OrderItemsRepository {
 
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    public Integer insertOrderItems(OrderItem orderItems) {
+        String insertSql = "INSERT INTO order_items(item_id, order_id, quantity, size) VALUES(:itemId, :orderId, :quantity, :size) RETURNING id";
+        SqlParameterSource param = new MapSqlParameterSource().addValue("itemId", orderItems.getItemId())
+                .addValue("orderId", orderItems.getOrderId()).addValue("quantity", orderItems.getQuantity())
+                .addValue("size", orderItems.getSize());
+        Integer id = template.queryForObject(insertSql, param, Integer.class);
+        return id;
+    }
+    
 }
