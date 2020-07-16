8997933bb530eef6ff03729c6a0a2970dcc1837b
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-07-16 10:05:27 +0900

    add method of insert for add shoppingCart

--- src/main/java/com/example/repository/OrderToppingsRepository.java
+++ src/main/java/com/example/repository/OrderToppingsRepository.java
@@ -1,5 +1,23 @@
 package com.example.repository;
 
+import com.example.domain.OrderTopping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class OrderToppingsRepository {
 
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    public void insertOrderToppings(OrderTopping orderTopping) {
+        String insertSql = "INSERT INTO order_toppings(topping_id, order_item_id) VALUES(:toppingId, :orderItemId)";
+        SqlParameterSource param = new MapSqlParameterSource().addValue("toppingId", orderTopping.getToppingId())
+                .addValue("orderItemId", orderTopping.getOrderItemId());
+        template.update(insertSql, param);
+    }
 }
