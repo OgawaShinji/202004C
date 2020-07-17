fcc395d221de4fd51c11dfe0453e1fb0aaabcf9e
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-07-17 13:50:52 +0900

    change login function for shoppingCart

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -10,13 +10,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Order;
 import com.example.domain.User;
 import com.example.form.LoginForm;
 import com.example.service.LoginService;
+import com.example.service.ShoppingCartService;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class LoginController {
+
+	@Autowired
+	private ShoppingCartService shoppingCartService;
 
 	@Autowired
 	private LoginService loginService;
@@ -29,37 +34,59 @@ public class LoginController {
 		return new LoginForm();
 	}
 
-/////////////////////////////////////////////////////
-// ユースケース：ログインをする
-/////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////
+	// ユースケース：ログインをする
+	/////////////////////////////////////////////////////
 
-/**
-* ログイン画面を出力します.
-* 
-* @return ログイン画面
-*/
-  @RequestMapping("/toLogin")
-  public String toLogin() {
-	  return "user/login";
-  }
-  /**
-   * ログインします
-   * @param loginForm
-   * @param model
-   * @return ログイン後小一覧画面
-   */
-  @RequestMapping("/login")
-  public String login(LoginForm loginForm, Model model) {
-	 User user = loginService.findByMailAddressAndPassward(loginForm.getMailAddress(), loginForm.getPassword());
-	 System.out.println(user);
-	 if (Objects.isNull(user)) {
-		model.addAttribute("failed", "メールアドレスまたはパスワードが違います");
-		return toLogin();
-	}else{
-		session.setAttribute("user",user);
-		//IndexControllerに処理移行をお願いします。
-		return "forword:/item/item_list";
+	/**
+	 * ログイン画面を出力します.
+	 * 
+	 * @return ログイン画面
+	 */
+	@RequestMapping("/toLogin")
+	public String toLogin() {
+		return "user/login";
 	}
-  }
+
+	/**
+	 * ログインします
+	 * 
+	 * @param loginForm
+	 * @param model
+	 * @return ログイン後小一覧画面
+	 */
+	@RequestMapping("/login")
+	public String login(LoginForm loginForm, Model model) {
+		User user = loginService.findByMailAddressAndPassward(loginForm.getMailAddress(), loginForm.getPassword());
+		System.out.println(user);
+		if (Objects.isNull(user)) {
+			model.addAttribute("failed", "メールアドレスまたはパスワードが違います");
+			return toLogin();
+		} else {
+			if (Objects.nonNull(session.getAttribute("user"))) {
+				User userAlreadyInSession = (User) session.getAttribute("user");
+
+				Order orderForLoginUser = new Order();
+				orderForLoginUser.setStatus(0);
+				orderForLoginUser.setUserId(user.getId());
+				Integer ordersIdForLoginUser = shoppingCartService.findIdByUserId(orderForLoginUser);
+
+				Order orderForNotLoginUser = new Order();
+				orderForNotLoginUser.setStatus(0);
+				orderForNotLoginUser.setUserId(userAlreadyInSession.getId());
+				Integer ordersIdForNotLoginUser = shoppingCartService.findIdByUserId(orderForLoginUser);
+
+				if (Objects.isNull(ordersIdForLoginUser)) {
+					shoppingCartService.updateOrdersUserId(userAlreadyInSession.getId(), orderForLoginUser);
+				} else {
+					orderForLoginUser.setId(ordersIdForLoginUser);
+					orderForNotLoginUser.setId(ordersIdForNotLoginUser);
+					shoppingCartService.changeUserDuringShopping(orderForNotLoginUser, orderForLoginUser);
+				}
+			}
+			session.setAttribute("user", user);
+			return "redirect:/item-list";
+		}
+	}
 
 }
