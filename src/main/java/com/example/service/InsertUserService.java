ed6eca939f8d3f4ed7acb3a7ae656ff0c4e99e42
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-07-17 11:00:11 +0900

    Comment out where encord part

--- src/main/java/com/example/service/InsertUserService.java
+++ src/main/java/com/example/service/InsertUserService.java
@@ -2,8 +2,8 @@ package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,23 +28,23 @@ public class InsertUserService {
 	 * 
 	 * @return bcryptアルゴリズムで暗号化する実装オブジェクト
 	 */
-	@Autowired
-	PasswordEncoder passwordEncoder;
+	// @Autowired
+	// PasswordEncoder passwordEncoder;
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	// @Bean
+	// PasswordEncoder passwordEncoder() {
+	// 	return new BCryptPasswordEncoder();
+	// }
 
 	/**
 	 *ユーザー情報を登録します. 
 	 * パスワードはここでハッシュ化されます
 	 * @param administrator 管理者情報
 	 */
-	public void insert(User user) {
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		usersRepository.insert(user);
-	}
+	// public void insert(User user) {
+	// 	user.setPassword(passwordEncoder.encode(user.getPassword()));
+	// 	usersRepository.insert(user);
+	// }
 
 	public User findByMailAddress(String email) {
 		return usersRepository.findByMailAddress(email);
