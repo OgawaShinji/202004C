48a9ed980a3e868d0de9ae5c141701200363f870
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-07-19 08:53:20 +0900

    fix error for getTax and getCalcTotalPrice

--- src/main/java/com/example/domain/Order.java
+++ src/main/java/com/example/domain/Order.java
@@ -19,90 +19,119 @@ public class Order {
 	private Integer paymentMethod;
 	private User user;
 	private List<OrderItem> orderItemList;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
+
 	public Integer getStatus() {
 		return status;
 	}
+
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
+
 	public Integer getTotalPrice() {
 		return totalPrice;
 	}
+
 	public void setTotalPrice(Integer totalPrice) {
 		this.totalPrice = totalPrice;
 	}
+
 	public Date getOrderDate() {
 		return orderDate;
 	}
+
 	public void setOrderDate(Date orderDate) {
 		this.orderDate = orderDate;
 	}
+
 	public String getDestinationName() {
 		return destinationName;
 	}
+
 	public void setDestinationName(String destinationName) {
 		this.destinationName = destinationName;
 	}
+
 	public String getDestinationEmail() {
 		return destinationEmail;
 	}
+
 	public void setDestinationEmail(String destinationEmail) {
 		this.destinationEmail = destinationEmail;
 	}
+
 	public String getDestinationZipcode() {
 		return destinationZipcode;
 	}
+
 	public void setDestinationZipcode(String destinationZipcode) {
 		this.destinationZipcode = destinationZipcode;
 	}
+
 	public String getDestinationAddress() {
 		return destinationAddress;
 	}
+
 	public void setDestinationAddress(String destinationAddress) {
 		this.destinationAddress = destinationAddress;
 	}
+
 	public String getDestinationTel() {
 		return destinationTel;
 	}
+
 	public void setDestinationTel(String destinationTel) {
 		this.destinationTel = destinationTel;
 	}
+
 	public Timestamp getDeliveryTime() {
 		return deliveryTime;
 	}
+
 	public void setDeliveryTime(Timestamp deliveryTime) {
 		this.deliveryTime = deliveryTime;
 	}
+
 	public Integer getPaymentMethod() {
 		return paymentMethod;
 	}
+
 	public void setPaymentMethod(Integer paymentMethod) {
 		this.paymentMethod = paymentMethod;
 	}
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
+
 	public List<OrderItem> getOrderItemList() {
 		return orderItemList;
 	}
+
 	public void setOrderItemList(List<OrderItem> orderItemList) {
 		this.orderItemList = orderItemList;
 	}
+
 	@Override
 	public String toString() {
 		return "Order [id=" + id + ", userId=" + userId + ", status=" + status + ", totalPrice=" + totalPrice
@@ -111,12 +140,14 @@ public class Order {
 				+ destinationAddress + ", destinationTel=" + destinationTel + ", deliveryTime=" + deliveryTime
 				+ ", paymentMethod=" + paymentMethod + ", user=" + user + ", orderItemList=" + orderItemList + "]";
 	}
-	public int getTax(int totalPrice) {
-		double taxPrice=totalPrice*0.1;
-		int returnPrice=(int)taxPrice;
+
+	public int getTax() {
+		double taxPrice = this.totalPrice * 0.1;
+		int returnPrice = (int) taxPrice;
 		return returnPrice;
 	}
-	public int getCalcTotalPrice(int totalPrice) {
-		return totalPrice+getTax(totalPrice);
+
+	public int getCalcTotalPrice() {
+		return this.totalPrice + getTax();
 	}
 }
