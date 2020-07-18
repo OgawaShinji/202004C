bb67494436152564a3ca1eafe2e9fa46c6a943be
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2020-07-18 23:54:39 +0900

    競合を削除

--- src/main/java/com/example/controller/ShoppingCartController.java
+++ src/main/java/com/example/controller/ShoppingCartController.java
@@ -18,12 +18,9 @@ import com.example.service.ShoppingHistoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-=======
 import org.springframework.ui.Model;
->>>>>>> feature/viwShoppingList
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
