09d62887f9b12bb750fc5f3917020f0c55f29cee
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2020-07-20 13:47:20 +0900

    test

---
+++ src/test/java/com/example/form/IndexFormTest.java
@@ -0,0 +1,20 @@
+package com.example.form;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//springのtestを行う時は必須
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IndexFormTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
