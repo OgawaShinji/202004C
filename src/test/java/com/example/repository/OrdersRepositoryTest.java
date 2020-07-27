8c37041e1c86be0b34d3e1e06d720d5c870343f6
sawaguchiarisa <arisa.sawaguchi@rakus-partners.co.jp>
2020-07-27 16:01:26 +0900

    微々

--- src/test/java/com/example/repository/OrdersRepositoryTest.java
+++ src/test/java/com/example/repository/OrdersRepositoryTest.java
@@ -9,9 +9,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.ninja_squad.dbsetup.Operations;
+import com.ninja_squad.dbsetup.operation.Operation;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OrdersRepositoryTest {
+	
+	public static final Operation DELETE_ALL = Operations.deleteAllFrom("items");
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
